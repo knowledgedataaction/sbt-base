6c8b0de76299312d95b98627d240e9793048ed38
Michael Reynolds <michael.reynolds@twosixlabs.com>
2019-05-29 18:18:11 -0400

    upgrading logback

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
 
   val slf4jVersion = "1.7.20"
-  val logbackVersion = "1.1.7"
+  val logbackVersion = "1.2.3"
   val scalaTestVersion = "3.0.5"
   
   val slf4j = Seq( "org.slf4j" % "slf4j-api" % slf4jVersion )
